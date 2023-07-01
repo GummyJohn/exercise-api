5e7a09f1c1ee8664029a7f3cb8823eb5a05ce6cb
Johnny <john@Richies-iMac.local>
2023-06-30 20:56:24 -0400

    almost done

--- JS/index.js
+++ JS/index.js
@@ -1,40 +1,81 @@
-import { getData } from "./api.js";
+import { 
+  getData,
+  getTypes,
+} from "./api.js";
+
 import { 
   header,
-  search,
-  muscleDisplay,
   singleBoxes,
+  tabs
 } from "./statics.js";
 
 const body = document.querySelector('#body')
 
 body.append(header())
-body.append(search())
-body.append(muscleDisplay())
-// body.append(typesSearch())
-// body.append(typesDisplay())
+body.append(tabs())
 
-
+// MUSCLE TAB
 const input = document.querySelector('.search-bar');
 const displayContainer = document.querySelector('.display-container');
 const button = document.querySelector('.button')
 
-// const typeInput = document.querySelector('.type-search-bar');
-// const typeDisplayContainer = document.querySelector('.type-display-container');
+const tab1 = document.querySelector('.muscle-tab')
+const muscleContent = document.querySelector('.content-box')
+const tab2 = document.querySelector('.types-tab');
+const typeContent = document.querySelector('.content-box-two');
+
+window.addEventListener('load', () => {
+  tab1.click();
+})
+
+tab1.addEventListener('click', () => {
+  muscleContent.classList.remove('hide');
+  typeContent.classList.add('hide');
+})
+
+tab2.addEventListener('click', () => {
+  typeContent.classList.remove('hide');
+  muscleContent.classList.add('hide');
+})
 
 input.addEventListener('keypress', async () => {
   if(event.keyCode === 13){
-    console.log(input.value)
     let getWorkouts = await getData(input.value);
-  
+    
     for(let i = 0; i < getWorkouts.length; i++){
-      console.log(i)
       displayContainer.append(singleBoxes(getWorkouts[i]));
     }
-
+    
+    input.value = '';
   }
+  
+  const contents = document.querySelectorAll('.box-container');
+  button.addEventListener('click', () => {
+    contents.forEach((each) => {
+      each.classList.add('hide');
+    })
+  })
 })
 
-button.addEventListener('click', () => {
-  console.log('refreshed')
+
+// TYPES TABS
+const buttonTwo = document.querySelector('.button-two');
+const typeInput = document.querySelector('.type-search-bar');
+const typeDisplayContainer = document.querySelector('.type-display-container');
+
+
+typeInput.addEventListener('keypress', async () => {
+  if(event.keyCode === 13){
+    let getTypesData = await getTypes(typeInput.value);
+    
+    for(let i = 0; i < getTypesData.length; i++){
+      typeDisplayContainer.append(singleBoxes(getTypesData[i]));
+    }
+    
+    typeInput.value = '';
+  }
+
+  buttonTwo.addEventListener('click', () => {
+      
+  })
 })
