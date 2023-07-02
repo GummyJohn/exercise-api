c14acb95fe28f8022206e3d3e84952733cab5d00
Johnny <john@Richies-iMac.local>
2023-07-02 19:43:01 -0400

    done

--- JS/index.js
+++ JS/index.js
@@ -6,22 +6,30 @@ import {
 import { 
   header,
   singleBoxes,
-  tabs
+  tabs,
 } from "./statics.js";
 
 const body = document.querySelector('#body')
+const innerBody = document.createElement('div');
+innerBody.className = 'inner-body';;
 
-body.append(header())
-body.append(tabs())
+innerBody.append(header())
+innerBody.append(tabs())
+
+body.append(innerBody);
 
 // MUSCLE TAB
 const input = document.querySelector('.search-bar');
 const displayContainer = document.querySelector('.display-container');
-const button = document.querySelector('.button')
+const typesDispayContainer = document.querySelector('.type-display-container')
+
+const button = document.querySelector('.button');
+const buttonTwo = document.querySelector('.button-two');
 
 const tab1 = document.querySelector('.muscle-tab')
-const muscleContent = document.querySelector('.content-box')
 const tab2 = document.querySelector('.types-tab');
+
+const muscleContent = document.querySelector('.content-box')
 const typeContent = document.querySelector('.content-box-two');
 
 window.addEventListener('load', () => {
@@ -43,39 +51,43 @@ input.addEventListener('keypress', async () => {
     let getWorkouts = await getData(input.value);
     
     for(let i = 0; i < getWorkouts.length; i++){
-      displayContainer.append(singleBoxes(getWorkouts[i]));
+      displayContainer.append(singleBoxes(getWorkouts[i],'muscle'));
     }
     
     input.value = '';
   }
   
-  const contents = document.querySelectorAll('.box-container');
+  const contents = document.querySelectorAll('.box-container-muscle');
+
   button.addEventListener('click', () => {
-    contents.forEach((each) => {
-      each.classList.add('hide');
-    })
+    for(let i = 0; i < contents.length; i++){
+      contents[i].classList.add('hide');
+    }
   })
+
 })
 
 
 // TYPES TABS
-const buttonTwo = document.querySelector('.button-two');
 const typeInput = document.querySelector('.type-search-bar');
-const typeDisplayContainer = document.querySelector('.type-display-container');
-
 
 typeInput.addEventListener('keypress', async () => {
   if(event.keyCode === 13){
     let getTypesData = await getTypes(typeInput.value);
     
     for(let i = 0; i < getTypesData.length; i++){
-      typeDisplayContainer.append(singleBoxes(getTypesData[i]));
+      typesDispayContainer.append(singleBoxes(getTypesData[i],'type'));
     }
     
     typeInput.value = '';
   }
 
+  const typeContents = document.querySelectorAll('.box-container-type');
+
   buttonTwo.addEventListener('click', () => {
-      
+    for(let i = 0; i < typeContents.length; i++){
+      typeContents[i].classList.add('hide');
+    }
   })
+
 })
