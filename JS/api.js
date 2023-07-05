32bbd228afabb5957334ce5f3c0573b23fb43087
GummyJohn <130239183+GummyJohn@users.noreply.github.com>
2023-07-04 20:42:24 -0400

    Update api.js

--- JS/api.js
+++ JS/api.js
@@ -2,10 +2,10 @@ export async function getData(text){
   const url = `https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=${text}`;
 
   const options = {
-    headers: {
-      'X-RapidAPI-Key': 'use your own API-KEY',
-      'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
-    }
+	headers: {
+	'X-RapidAPI-Key': 'use your own API-KEY',
+	'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
+	}
   };
   
   
